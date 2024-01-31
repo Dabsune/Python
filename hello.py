67e60ad40ef9a0a52ecb7d4a8aa97548ad4a2d61
Kephren Bibang <kephren.bib@gmail.com>
2024-01-31 12:33:23 +0100

    Question/réponse opérationnelle

--- hello.py
+++ hello.py
@@ -1,1 +1,5 @@
-nombre = input("Saisis un nombre, n'importe lequel : ")
+nbr = int(input("Saisis un nombre, n'importe lequel : "))
+print("Dans l'ensemble infini de ℝ, tu as choisi " + str(nbr) + ". Intéressant...")
+print("Voici sa table de multiplication de 1 à 10, comme à l'école :")
+for x in range(1, 11):
+    print(nbr, "×", x, "=", nbr * x)
